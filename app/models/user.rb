6db2dcdb1adb177845a2c6c99e7062c1ae111f31
evShun <124552406+evShun@users.noreply.github.com>
2023-03-06 01:28:07 +0900

    write validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-zA-Z\d]+\z/ }
+
+  validates :nickname         , presence: true
+  validates :family_name      , presence: true , format: { with:/\A[ぁ-んァ-ン一-龥]/ ,message: '全角文字を使用してください'}
+  validates :first_name       , presence: true , format: { with:/\A[ぁ-んァ-ン一-龥]/ ,message: '全角文字を使用してください'}
+  validates :family_name_kana , presence: true , format: { with:/\A[ァ-ヴー]+\z/u ,message: '全角カタカナを使用してください'}
+  validates :first_name_kana  , presence: true , format: { with:/\A[ァ-ヴー]+\z/u ,message: '全角カタカナを使用してください'}
+  validates :birthday         , presence: true
+
 end
