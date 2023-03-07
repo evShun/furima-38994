ecc75cbe700a43285103111b6a83e37c72341dc9
evShun <124552406+evShun@users.noreply.github.com>
2023-03-07 18:10:10 +0900

    install Active Storage

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+
+  validates :title	       , presence: true
+  validates :text          , presence: true
+  validates :category_id   , presence: true
+  validates :status_id     , presence: true
+  validates :charge_id     , presence: true
+  validates :prefecture_id , presence: true
+  validates :shipping_id   , presence: true
+  validates :price         , presence: true
+  validates :user          , presence: true
+  # imageはどうする？
+    
+  belongs_to :users
+  has_one_attached :image
+
 end
