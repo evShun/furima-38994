80b3472487478d858ae72f5647ecafe8775df7e3
evShun <124552406+evShun@users.noreply.github.com>
2023-03-08 17:41:43 +0900

    Active hash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,23 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :shipping
+  belongs_to :users
+  has_one_attached :image
 
   validates :title	       , presence: true ,length: { maximum: 40 }
   validates :text          , presence: true
-  validates :category_id   , presence: true
-  validates :status_id     , presence: true
-  validates :charge_id     , presence: true
-  validates :prefecture_id , presence: true
-  validates :shipping_id   , presence: true
+  validates :category_id   , presence: true ,numericality: { other_than: 0 ,message: "can't be blank"}
+  validates :status_id     , presence: true ,numericality: { other_than: 0 ,message: "can't be blank"}
+  validates :charge_id     , presence: true ,numericality: { other_than: 0 ,message: "can't be blank"}
+  validates :prefecture_id , presence: true ,numericality: { other_than: 0 ,message: "can't be blank"}
+  validates :shipping_id   , presence: true ,numericality: { other_than: 0 ,message: "can't be blank"}
   validates :price         , presence: true # { with:/\A[/d]+\z/ ,message: '半角を使用してください'} jconに書く？
   validates :user          , presence: true
   # imageはどうする？
-    
-  belongs_to :users
-  has_one_attached :image
+  
 
 end
