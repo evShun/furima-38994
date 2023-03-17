e4ca1712aa5fc33b4900afd8c7de7595fe2050ba
evShun <124552406+evShun@users.noreply.github.com>
2023-03-17 20:23:27 +0900

    only buyer can acsses order/index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,9 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item
+  before_action :sale_chack
 
   def index
-    @item = Item.find(params[:item_id])
     @order = Order.new
     unless user_signed_in? && current_user.id != @item.user.id
       redirect_to root_path
@@ -10,7 +11,6 @@ class OrdersController < ApplicationController
   end
   
   def create
-    @item = Item.find(params[:item_id])
     @order = Order.new(set_params)
     if @order.valid?
       pay_item
@@ -23,13 +23,25 @@ class OrdersController < ApplicationController
 end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def sale_chack
+    unless @item.purchase.blank?  
+      redirect_to root_path
+    end
+    if current_user.id == @item.user.id
+      redirect_to root_path
+    end
+  end
+
   def set_params
-    params.require(:order).permit(:postal_code,:prefecture_id,:city,:number,:building,:phone_number,:purchase,:item_id).merge(user_id: current_user.id, token: params[:token] )
+    params.require(:order).permit(:postal_code,:prefecture_id,:city,:number,:building,:phone_number,:purchase,).merge(user_id: current_user.id, item_id: @item.id,token: params[:token] )
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(amount: @item[:price], card: params[:token], currency: 'jpy')
   end
-
-
