93fd3d78d5adce4778fe571415db14aea3faaa50
evShun <124552406+evShun@users.noreply.github.com>
2023-03-17 19:53:59 +0900

    implement view errormassage

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,19 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @item = Item.find(params[:item_id])
+    @order = Order.new
+    unless user_signed_in? && current_user.id != @item.user.id
+      redirect_to root_path
+    end
   end
   
   def create
     @item = Item.find(params[:item_id])
     @order = Order.new(set_params)
-    pay_item
     if @order.valid?
+      pay_item
       @order.save
       redirect_to root_path
     else
@@ -19,7 +24,7 @@ end
 
   private
   def set_params
-    params.permit(:postal_code,:prefecture_id,:city,:number,:building,:phone_number,:purchase,:item_id).merge(user_id: current_user.id, token: params[:token] )
+    params.require(:order).permit(:postal_code,:prefecture_id,:city,:number,:building,:phone_number,:purchase,:item_id).merge(user_id: current_user.id, token: params[:token] )
   end
 
   def pay_item
