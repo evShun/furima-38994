b1536b6cea74e5f3247160334292ac62d4aa3399
evShun <124552406+evShun@users.noreply.github.com>
2023-03-17 19:53:02 +0900

    write testcode

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,11 +3,11 @@ class Order
   attr_accessor :postal_code,:prefecture_id,:city,:number,:building,:phone_number,:user_id,:item_id,:token
 
   with_options presence: true do
-   validates :postal_code   ,format: { with:/\A[0-9]{3}-[0-9]{4}\z/ ,message: '000-0000の形式に変更してください'}
+   validates :postal_code   ,format: { with:/\A[0-9]{3}-[0-9]{4}\z/,message: '000-0000の形式に変更してください'}
    validates :prefecture_id ,numericality: { other_than: 0, message: "can't be blank" }
    validates :city
    validates :number
-   validates :phone_number  ,format: { with: /\A[0-9]+\z/ message: '半角で入力してください'} ,length:{ minimum: 10, maximum: 11 }
+   validates :phone_number  ,format: { with:/\A[0-9]+\z/, message: '半角で入力してください'} ,length:{ minimum: 10, maximum: 11 }
    validates :token
    validates :user_id
    validates :item_id
---
+++ spec/models/order_spec.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order = FactoryBot.build(:order, user_id: user.id, item_id: item.id)
+  end
+
+describe '商品購入' do
+  context '商品購入できるとき' do
+    it '空欄を埋めると購入できる' do
+      expect(@order).to be_valid
+    end
+  end
+
+
+  context '購入できないとき' do
+    it 'postal_codeが空では購入できない' do
+      @order.postal_code = ''
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Postal code can't be blank")
+    end
+    it 'postal_codeに-が含まれていないと購入できない' do
+      @order.postal_code = '1234567'
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Postal code 000-0000の形式に変更してください")
+    end
+
+    it 'prefecture_idが空では購入できない' do
+      @order.prefecture_id = ''
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Prefecture can't be blank")
+    end
+    it 'prefecture_idが0では購入できない' do
+      @order.prefecture_id = '0'
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Prefecture can't be blank")
+    end
+
+    it 'cityが空では購入できない' do
+      @order.city = ''
+      @order.valid?
+      expect(@order.errors.full_messages).to include("City can't be blank")
+    end
+
+    it 'numberが空では購入できない' do
+      @order.number = ''
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Number can't be blank")
+    end
+
+    it 'phone_numberが空では購入できない' do
+      @order.phone_number = ''
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Phone number can't be blank")
+    end
+    it 'phone_numberが全角では購入できない' do
+      @order.phone_number = '０１２３４５６７８９'
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Phone number 半角で入力してください")
+    end
+
+    it 'tokenが空では購入できない' do
+      @order.token = ''
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Token can't be blank")
+    end
+
+    it 'ユーザーが紐付いていなければ投稿できない' do
+      @order.user_id = nil
+      @order.valid?
+      expect(@order.errors.full_messages).to include("User can't be blank")
+    end
+
+    it '商品が紐付いていなければ投稿できない' do
+      @order.item_id = nil
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Item can't be blank")
+    end
+  end
+end
+
+end
